a3e8f7270f48c7cb50dab60f560612ffdc39dc67
Simo <a>
2017-04-24 18:49:08 +0300

    commit

--- JavaScriptCode.js
+++ JavaScriptCode.js
@@ -1,3 +1,27 @@
 /**
  * Created by Simo on 24/04/2017.
  */
+$( document ).ready(function() {
+    pageLoaded();
+});
+
+function pageLoaded ()
+{
+    ShowSection('Welcome');
+}
+function ShowSection(id)
+{
+    //hide all
+    var Welcome = document.getElementById('Welcome');
+    Welcome.style.visibility="hidden";
+    var Register = document.getElementById('Register');
+    Register.style.visibility="hidden";
+    var Login = document.getElementById('Login');
+    Login.style.visibility="hidden";
+    var About = document.getElementById('About');
+    About.style.visibility="hidden";
+
+    //Show selected
+    var selected = document.getElementById(id);
+    selected.style.visibility= "visible";
+}
