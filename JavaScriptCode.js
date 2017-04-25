48f51d1665d8891ef8c00affdc601c0a68062c0b
stavlut <stavlut@gmail.com>
2017-04-25 15:03:21 +0300

    fgh

--- JavaScriptCode.js
+++ JavaScriptCode.js
@@ -8,7 +8,9 @@ $( document ).ready(function() {
 });
 //Game vars
 
-var board;
+var staticBoard;
+var  dynamicBoard;
+
 var pacman;
 
 function pacman(x,y)
@@ -31,9 +33,9 @@ function moveCharacter(direction,character)
         {
             if(checkIfMoveIsValid(character.x,character.y-1))
             {
-                board[character.x][character.y] = 0;
+                staticBoard[character.x][character.y] = 0;
                 character.y = character.y-1;
-                board[character.x][character.y] = character.code;
+                staticBoard[character.x][character.y] = character.code;
             }
             break;
         }
@@ -41,9 +43,9 @@ function moveCharacter(direction,character)
         {
             if(checkIfMoveIsValid(character.x,character.y+1))
             {
-                board[character.x][character.y] = 0;
+                staticBoard[character.x][character.y] = 0;
                 character.y = character.y+1;
-                board[character.x][character.y] = character.code;
+                staticBoard[character.x][character.y] = character.code;
             }
 
             break;
@@ -52,9 +54,9 @@ function moveCharacter(direction,character)
         {
             if(checkIfMoveIsValid(character.x-1,character.y))
             {
-                board[character.x][character.y] = 0;
+                staticBoard[character.x][character.y] = 0;
                 character.x = character.x-1;
-                board[character.x][character.y] = character.code;
+                staticBoard[character.x][character.y] = character.code;
             }
             break;
         }
@@ -62,9 +64,9 @@ function moveCharacter(direction,character)
         {
             if(checkIfMoveIsValid(character.x+1,character.y))
             {
-                board[character.x][character.y] = 0;
+                staticBoard[character.x][character.y] = 0;
                 character.x = character.x+1;
-                board[character.x][character.y] = character.code;
+                staticBoard[character.x][character.y] = character.code;
             }
             break;
         }
@@ -72,7 +74,7 @@ function moveCharacter(direction,character)
 }
 function checkIfMoveIsValid(x,y)
 {
-    if(board[x][y] == 1 || x<0 || x>12 || y<0 || y>17)
+    if(staticBoard[x][y] == 1 || x<0 || x>12 || y<0 || y>17)
     {
         return false;
     }
@@ -80,91 +82,91 @@ function checkIfMoveIsValid(x,y)
 }
 
 function buildBoard() {
-    board = new Array();
+    staticBoard = new Array();
     for (var i = 0; i < 18; i++) {
-        board[i] = new Array();
+        staticBoard[i] = new Array();
         for (var j = 0; j < 13; j++) {
-            board[i][j] = 0;
+            staticBoard[i][j] = 0;
         }
     }
         for (var j = 0; j < 13; j++) {
-            board[0][j] = 1;
+            staticBoard[0][j] = 1;
         }
          for (var j = 0; j < 13; j++) {
-            board[17][j] = 1;
+            staticBoard[17][j] = 1;
          }
 
         for (var j = 0; j < 18; j++) {
-            board[j][0] = 1;
+            staticBoard[j][0] = 1;
         }
         for (var j = 0; j < 18; j++) {
-            board[j][12] = 1;
+            staticBoard[j][12] = 1;
         }
-    board[2][2] = 1;
-    board[2][3] = 1;
-    board[3][3] = 1;
-    board[3][4] = 1;
-    board[3][5] = 1;
-    board[4][5] = 1;
-    board[4][1] = 1;
-    board[1][5] = 1;
-    board[1][7] = 1;
-    board[2][10] = 1;
-    board[3][7] = 1;
-    board[3][8] = 1;
-    board[4][7] = 1;
-    board[4][8] = 1;
-    board[4][9] = 1;
-    board[4][11] = 1;
-    board[5][11] = 1;
-    board[5][8] = 1;
-    board[5][9] = 1;
-    board[6][8] = 1;
-    board[7][8] = 1;
-    board[5][3] = 1;
-    board[6][2] = 1;
-    board[5][9] = 1;
-    board[6][8] = 1;
-    board[7][8] = 1;
-    board[6][5] = 1;
-    board[6][6] = 1;
-    board[7][6] = 1;
-    board[8][6] = 1;
-    board[9][6] = 1;
-    board[10][6] = 1;
-    board[11][6] = 1;
-    board[11][5] = 1;
-    board[10][8] = 1;
-    board[11][8] = 1;
-    board[12][8] = 1;
-    board[13][8] = 1;
-    board[14][8] = 1;
-    board[12][9] = 1;
-    board[13][9] = 1;
-    board[13][7] = 1;
-    board[14][7] = 1;
-    board[7][4] = 1;
-    board[10][4] = 1;
-    board[10][2] = 1;
-    board[11][2] = 1;
-    board[12][3] = 1;
-    board[13][1] = 1;
-    board[7][2] = 1;
-    board[15][2] = 1;
-    board[15][3] = 1;
-    board[14][3] = 1;
-    board[14][4] = 1;
-    board[14][5] = 1;
-    board[13][5] = 1;
-    board[7][10] = 1;
-    board[8][10] = 1;
-    board[9][10] = 1;
-    board[10][10] = 1;
-    board[16][5] = 1;
-    board[16][7] = 1;
-    board[15][10] = 1;
-    board[12][11] = 1;
-    board[13][11] = 1;
+    staticBoard[2][2] = 1;
+    staticBoard[2][3] = 1;
+    staticBoard[3][3] = 1;
+    staticBoard[3][4] = 1;
+    staticBoard[3][5] = 1;
+    staticBoard[4][5] = 1;
+    staticBoard[4][1] = 1;
+    staticBoard[1][5] = 1;
+    staticBoard[1][7] = 1;
+    staticBoard[2][10] = 1;
+    staticBoard[3][7] = 1;
+    staticBoard[3][8] = 1;
+    staticBoard[4][7] = 1;
+    staticBoard[4][8] = 1;
+    staticBoard[4][9] = 1;
+    staticBoard[4][11] = 1;
+    staticBoard[5][11] = 1;
+    staticBoard[5][8] = 1;
+    staticBoard[5][9] = 1;
+    staticBoard[6][8] = 1;
+    staticBoard[7][8] = 1;
+    staticBoard[5][3] = 1;
+    staticBoard[6][2] = 1;
+    staticBoard[5][9] = 1;
+    staticBoard[6][8] = 1;
+    staticBoard[7][8] = 1;
+    staticBoard[6][5] = 1;
+    staticBoard[6][6] = 1;
+    staticBoard[7][6] = 1;
+    staticBoard[8][6] = 1;
+    staticBoard[9][6] = 1;
+    staticBoard[10][6] = 1;
+    staticBoard[11][6] = 1;
+    staticBoard[11][5] = 1;
+    staticBoard[10][8] = 1;
+    staticBoard[11][8] = 1;
+    staticBoard[12][8] = 1;
+    staticBoard[13][8] = 1;
+    staticBoard[14][8] = 1;
+    staticBoard[12][9] = 1;
+    staticBoard[13][9] = 1;
+    staticBoard[13][7] = 1;
+    staticBoard[14][7] = 1;
+    staticBoard[7][4] = 1;
+    staticBoard[10][4] = 1;
+    staticBoard[10][2] = 1;
+    staticBoard[11][2] = 1;
+    staticBoard[12][3] = 1;
+    staticBoard[13][1] = 1;
+    staticBoard[7][2] = 1;
+    staticBoard[15][2] = 1;
+    staticBoard[15][3] = 1;
+    staticBoard[14][3] = 1;
+    staticBoard[14][4] = 1;
+    staticBoard[14][5] = 1;
+    staticBoard[13][5] = 1;
+    staticBoard[7][10] = 1;
+    staticBoard[8][10] = 1;
+    staticBoard[9][10] = 1;
+    staticBoard[10][10] = 1;
+    staticBoard[16][5] = 1;
+    staticBoard[16][7] = 1;
+    staticBoard[15][10] = 1;
+    staticBoard[12][11] = 1;
+    staticBoard[13][11] = 1;
 }
 
 function Draw() {
@@ -177,7 +179,7 @@ function Draw() {
             ctx.x = i * 10 + 10;
             ctx.y = j *10 + 10;
 
-           if (board[i][j] == 0) {
+           if (staticBoard[i][j] == 0) {
 
                ctx.beginPath();
                ctx.rect(ctx.x,ctx.y,10,10,5);
@@ -185,7 +187,7 @@ function Draw() {
                ctx.fill();
 
             }
-            else if (board[i][j] == 1) {
+            else if (staticBoard[i][j] == 1) {
                ctx.beginPath();
                ctx.rect(ctx.x,ctx.y,10,10,5);
                ctx.fillStyle = "red";
@@ -208,19 +210,19 @@ function fillPointsBalls(numberOfPointsBalls) {
                 {
                     if(ball5points > 0)
                     {
-                        board[j][i] = 6;
+                        staticBoard[j][i] = 6;
                         ball5points--;
                         counter--;
                     }
                     else if(ball15points > 0 )
                     {
-                        board[j][i] = 7;
+                        staticBoard[j][i] = 7;
                         ball15points--;
                         counter--;
                     }
                     else
                     {
-                        board[j][i] = 8;
+                        staticBoard[j][i] = 8;
                         bal25points--;
                         counter--;
                     }
