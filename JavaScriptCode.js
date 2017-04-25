1408ba179e6f086c1fa526c1ec345299a8d3d8fb
Simo <a>
2017-04-25 14:10:23 +0300

    commit

--- JavaScriptCode.js
+++ JavaScriptCode.js
@@ -4,71 +4,85 @@
 $( document ).ready(function() {
     pageLoaded();
     buildBoard();
+    draw();
 });
-//Game functions
+//Game vars
+
 var board;
 
-function buildBoard() {
-    board = new Array();
-    for (var i = 0; i < 18; i++) {
-        board[i] = new Array();
-        for (var j = 0; j < 13; j++) {
-            board[i][j] = 0;
+
+
+//Game functions
+function Pacman(posX,posY,img) {
+    this.posX = posX;
+    this.posY = posY;
+    this.img = new Image();
+    this.img.src = img;
+    
+}
+function draw()
+{
+    var c = document.getElementById("myCanvas");
+    var content = c.getContext("2d");
+    for(var i = 0;i<13;i++)
+    {
+        for(var j = 0;j<18;j++)
+        {
+            switch(board[i][j])
+            {
+                case 0:
+                    content.fillRect(i*5,j*5,5,5);
+                    content.fillStyle="#4286f4";
+                    break;
+
+                case 1:
+
+                    content.fillStyle="#FF0000";
+                    content.fillRect(i*5,j*5,5,5);
+                    break;
+            }
         }
     }
-    board[0][0] = 1;
-    //horizontal outer
-    board[0][12] = 1;
-    // vertical outer
-    board[0][0] = 1;
-    board[0][7] = 1;
-    board[17][0] = 1;
-    board[17][7] = 1;
-    // ghost base
-    board[7][4] = 1;
-    board[6][5] = 1;
-    board[10][4] = 1;
-    board[11][5] = 1;
-    board[6][6] = 1;
-    // single blocks
-    board[4][0] = 1;
-    board[13][0] = 1;
-    board[2][2] = 1;
-    board[6][2] = 1;
-    board[15][2] = 1;
-    board[10][2] = 1;
-    board[2][3] = 1;
-    board[14][3] = 1;
-    board[5][3] = 1;
-    board[12][3] = 1;
-    board[3][3] = 1;
-    board[14][3] = 1;
-    board[3][4] = 1;
-    board[14][4] = 1;
-    board[0][5] = 1;
-    board[3][5] = 1;
-    board[16][5] = 1;
-    board[13][5] = 1;
-    board[0][7] = 1;
-    board[16][7] = 1;
-    board[3][7] = 1;
-    board[13][7] = 1;
-    board[4][8] = 1;
-    board[12][8] = 1;
-    board[5][8] = 1;
-    board[10][8] = 1;
-    board[2][10] = 1;
-    board[15][10] = 1;
-    board[7][10] = 1;
-    board[4][11] = 1;
-    board[12][11] = 1;
-
-    /* ------------ End Pre-Build Walls  ------------ */
+}
+function fillPointsBalls(numberOfPointsBalls) {
+    var ball5points = 0.6*numberOfPointsBalls;
+    var ball15points = 0.3*numberOfPointsBalls;
+    var bal25points = 0.1*numberOfPointsBalls;
+    var counter = numberOfPointsBalls;
+    while(counter >0) {
+        for (var i = 0; i < 13; i++) {
+            for (var j = 0; j < 18; j++)
+            {
+                if(Math.random() > 0.5)
+                {
+                    if(ball5points > 0)
+                    {
+                        board[j][i] = 6;
+                        ball5points--;
+                        counter--;
+                    }
+                    else if(ball15points > 0 )
+                    {
+                        board[j][i] = 7;
+                        ball15points--;
+                        counter--;
+                    }
+                    else
+                    {
+                        board[j][i] = 8;
+                        bal25points--;
+                        counter--;
+                    }
+                }
+            }
+        }
+    }
 
 }
 
+function movePacman() {
 
-
+}
 //Layout functions
 function pageLoaded ()
 {
